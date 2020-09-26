3af911d51744a2fd737f9a1b3e7d99cc8e23e21d
Marcin Pastuszek <m.pastuszek@moj-sklep.pl>
2020-09-26 15:05:52 +0200

    Pierwsze zajęcia

--- 1_zmienne.php
+++ 1_zmienne.php
@@ -1,10 +1,53 @@
 <!DOCTYPE html>
-<html lang="[;]">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zmienne</title>
 </head>
 <body>
+    Pierwszy plik <hr>
+    <?php
+    $name = 'Anna';
+    // konkatenacja .
+    echo "Imię: $name";
 
+    // składnia heredoc
+    /* #1
+    $text = <<<LABEL
+        Imię: $name
+LABEL;
+    echo $text;
+
+    // #2
+    echo <<<L
+    Heredoc 2 <hr/>
+    L;
+    */
+
+    // składnia nowdoc
+    echo <<<'L'
+
+L;
+
+    // integer
+    $bin = 0b1010; //10
+    $oct = 011;     //9
+    $hex = 0x11; //17
+
+    $city = 'Poznań';
+    echo "Nazwa zmiennej: \$city, wartość: $city";
+
+    $x = true;
+
+    // wersja PHP
+    echo PHP_VERSION;
+
+
+
+
+
+
+    ?>
 </body>
 </html>
